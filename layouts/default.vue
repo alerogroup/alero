bfc00cee1be73dd6b5741658018866ee2c2a6dbe
Stephanie Kirathe <stephaniekirathe@gmail.com>
2020-05-28 20:53:37 +0300

    first

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-content>
-      <v-app-bar height="90" color="background">
+      <v-app-bar flat height="90" color="background">
         <img width="200" class="ml-12" :src="require('../static/icon.svg')">
       </v-app-bar>
       <v-content>
